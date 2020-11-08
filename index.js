52a9e0c508c90f531b33184dc610bc5d1fdef869
gsandconner <gsandconner@gmail.com>
2020-11-07 19:23:38 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -12,6 +12,32 @@ app.use(express.json())
 app.get("/hi",(req,res)=>{
     res.send("hello World")
 })
+app.post("/customer",(req,res)=>{
+    res.send("creating user")
+    console.log("request body", req.body)
+
+var CustomerFName =req.body.CustomerFName;
+var CustomerLName =req.body.CustomerLName;
+var CustomerEmail =req.body.CustomerEmail;
+var CustomerPassword=req.body.CustomerPassword;
+
+var emailCheckQuery=`SELECT CustomerEmail
+FROM Customer
+WHERE CustomerEmail='jondoe@Gmail.com'`
+
+var existingUser = await db.executeQuery(emailCheckQuery)
+
+console.log("existing user", existingUser)
+if(existingUser[0]){
+    return res.status(409).send('Please Enter a Different Email.')}
+var insertQuery =`INSERT INTO Customer(CustomerFName,CustomerLName,CustomerEmail,CustomerPassword,CustomerStreet,CustomerState,CustomerZip,CustomerPhone)
+VALUES('${CustomerFName}','${CustomerLName}','${CustomerEmail}','${CustomerPassword}','123 Dove Lane','CO','1234567','99999999')`
+db.executeQuery(insertQuery).then(()=>{res.status[201].send()})
+.catch((myError)=>{
+    console.log("ERROR")
+})
+
+})
 app.get("/stickers", (req,res)=>{
 db.executeQuery (`SELECT *
 FROM Sticker
